4f4b558f8128dd32376f9053baf0aebf20d91bbe
Kititach Chaiyasat <txzazalnw@gmail.com>
2025-11-19 15:58:26 +0700

    Update Program.cs

--- 11192025/Program.cs
+++ 11192025/Program.cs
@@ -1,37 +1,68 @@
-﻿namespace _11192025
+namespace _11192025
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            { 
             string name = "";
             int height = 0;
             string gender = "";
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine();
-            Console.Write("please, inpute you full name (ENG) :");
-            name = Console.ReadLine();
 
+             
+                while (true)
+            {
+                Console.Write("please, inpute you full name (ENG) :");
+                name = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    break;
+                }
+                Console.WriteLine("Name cannot be empty please try again.");
+            }
             Console.WriteLine("Hello, Welcome " + name);
-            Console.Write("Please input your gender (M/F): ");
-            gender = Console.ReadLine().ToUpper();
-            Console.Write("Please fill you height(cm) : ");
-            height = Convert.ToInt16(Console.ReadLine());
+            while (true)
+            {
+                Console.Write("Please input your gender (m/f): ");
+                gender = Console.ReadLine();
+
+                if (gender == "m" || gender == "f")
+                    break;
+                Console.WriteLine("Invalid gender code please enter 'm' or 'f'");
+            }
+            while (true)
+            {
+                Console.Write("Please fill you height(cm) : ");
+                string inputHeight = Console.ReadLine();
+
+                if (int.TryParse(inputHeight, out height) && height > 0)
+                    break;
+                Console.WriteLine("Invalid height! Must be a positive number.");
+            }
+
             double weight = 0;
-            if (gender == "M")
+            if (gender == "m")
             {
                 weight = height - 100;
-            } else if(gender == "F")
-                    { weight = height - 110;
-            }else
+            } else if (gender == "f")
+            { weight = height - 110;
+            } else
             {
                 Console.WriteLine("Invalid code");
             }
 
-                Console.WriteLine("You ideal weight is " + weight.ToString());
-            Console.WriteLine();
-            Console.WriteLine("Please any key to end program");
-            Console.ReadKey();
-        }
+        Console.WriteLine("You ideal weight is " + weight.ToString());
+        Console.WriteLine();
+
+        Console.Write("Press Q to quit or Enter to continue: ");
+        string exitCheck = Console.ReadLine();
+           if (exitCheck.ToLower() == "q")
+              {
+               Console.WriteLine("Program ended. Goodbye!");
+                    break;
+               }
+            }
+      }
     }
 }
